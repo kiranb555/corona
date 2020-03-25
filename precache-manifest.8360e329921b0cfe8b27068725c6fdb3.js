81792b50f967425eb570d79f7a8f7bfc401be3e4
kiranb555 <kiran.basavaraj05@gmail.com>
2020-03-26 00:16:15 +0530

    Updates

---
+++ precache-manifest.8360e329921b0cfe8b27068725c6fdb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2412acad962461701eab4dec17550f3",
+    "url": "/kiranb555/corona/index.html"
+  },
+  {
+    "revision": "ae2fffcec1ffe0f27bfb",
+    "url": "/kiranb555/corona/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5bbf64dffe12314ff356",
+    "url": "/kiranb555/corona/static/css/main.fba5c77d.chunk.css"
+  },
+  {
+    "revision": "ae2fffcec1ffe0f27bfb",
+    "url": "/kiranb555/corona/static/js/2.32fc8850.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/kiranb555/corona/static/js/2.32fc8850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bbf64dffe12314ff356",
+    "url": "/kiranb555/corona/static/js/main.a89c8085.chunk.js"
+  },
+  {
+    "revision": "facfabaaea64098c53b4",
+    "url": "/kiranb555/corona/static/js/runtime-main.5f51152c.js"
+  }
+]);
