f64e8d90f60af6bbe894280e6708286a1cd5d3f6
kiranb555 <kiran.basavaraj05@gmail.com>
2020-03-29 20:39:28 +0530

    removed middleware on production

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
-import {Provider } from 'react-redux';
-import { createStore,applyMiddleware } from "redux";
-import logger from 'redux-logger';
-import rootReducers from './reducers/rootReducers.js';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import rootReducers from "./reducers/rootReducers.js";
 
+const middleware = [];
 
-const middleware = applyMiddleware(logger);
+if (process.env.NODE_ENV === "development") {
+  middleware.push(logger);
+}
 
-const store = createStore(rootReducers,middleware);
+const store = createStore(rootReducers, applyMiddleware(...middleware));
 
-ReactDOM.render(<Provider store={store}><App /></Provider>,document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
